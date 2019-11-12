c24c465400412010035e4ed09027f14f8f30b1ea
reggie@bizbox.ph <reggiepangilinan@gmail.com>
2019-11-13 08:45:59 +1100

    Refactor checkbox style

--- example/src/components/checkbox.stories.tsx
+++ example/src/components/checkbox.stories.tsx
@@ -5,6 +5,20 @@ import React, { ReactElement } from 'react';
 storiesOf('Components/Checkbox', module)
   .add('Default', (): ReactElement => <Checkbox text="Are you in?" />)
   .add(
-    'Disabled',
+    'Disabled Empty',
     (): ReactElement => <Checkbox text={`Cant click me`} disabled />
+  )
+  .add(
+    'Disabled Checked',
+    (): ReactElement => <Checkbox text={`Cant click me`} disabled checked />
+  )
+  .add(
+    'With Error',
+    (): ReactElement => (
+      <Checkbox
+        text={`Cant click me`}
+        error={'Oops something is wrong.'}
+        touched
+      />
+    )
   );
