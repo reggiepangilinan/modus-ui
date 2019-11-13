988c92c3a7ae16b86e237585c72dfebb6b32342c
Reggie Pangilinan <reggiepangilinan@gmail.com>
2019-11-13 14:51:44 +1100

    Refactor checkbox style (#20)

--- example/src/components/table.stories.tsx
+++ example/src/components/table.stories.tsx
@@ -12,23 +12,7 @@ import {
 } from 'modus-ui';
 import React, { ReactElement } from 'react';
 
-const rows = new Array<ReactElement>();
-for (let index = 1; index < 200; index++) {
-  rows.push(
-    <TableRow key={index.toString()}>
-      <TableCell>John Doe {index}</TableCell>
-      <TableCell>10</TableCell>
-      <TableCell>The quick brown fox jumps over the lazy dog {index}</TableCell>
-      <TableCell align="right">
-        <IconButton
-          type={IconTypes.ellipsis}
-          transform={IconAngle.NegativeNinety}
-          size={IconSize.XSmall}
-        />
-      </TableCell>
-    </TableRow>
-  );
-}
+import { getLotsOfRows } from './getLotsOfRows';
 
 storiesOf('Components/Table', module)
   .add(
@@ -111,7 +95,7 @@ storiesOf('Components/Table', module)
             <TableHeader maxWidth="100px" label="" />
           </TableHead>
         }
-        rows={rows}
+        rows={getLotsOfRows()}
       />
     )
   )
