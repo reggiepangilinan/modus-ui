c9db52a6501ea8d45debaad9e84279fb5680ccd0
Reggie <reggie.pangilinan@infotrack.com.au>
2020-01-07 17:25:55 +1100

    Add SelectField basic stories

--- src/components/selectfield/selectfield.stories.tsx
+++ src/components/selectfield/selectfield.stories.tsx
@@ -29,39 +29,43 @@ selectFieldDefault.story = {
   name: 'Default'
 };
 
-// export const password = (): ReactElement => (
-//   <InputField
-//     label="Enter Password"
-//     type={'password'}
-//     placeholder="Placeholder"
-//     value="secrets"
-//   />
-// );
-// password.story = {
-//   name: 'Password'
-// };
+export const selectFieldLabel = (): ReactElement => (
+  <SelectField
+    name="selectFieldLabel"
+    label="With Label"
+    placeholder="Placeholder"
+    currentValue={undefined}
+    options={options}
+  />
+);
+selectFieldLabel.story = {
+  name: 'With Label'
+};
 
-// export const number = (): ReactElement => (
-//   <InputField
-//     label="This is number field"
-//     type={'number'}
-//     placeholder="Placeholder"
-//     value={100.01}
-//   />
-// );
-// number.story = {
-//   name: 'Number'
-// };
+export const selectFieldWithValue = (): ReactElement => (
+  <SelectField
+    name="selectFieldWithValue"
+    label="This one has value selected"
+    placeholder="Placeholder"
+    currentValue={{ value: 'chocolate', label: 'Chocolate' }}
+    options={options}
+  />
+);
+selectFieldWithValue.story = {
+  name: 'With Value Selected'
+};
 
-// export const withError = (): ReactElement => (
-//   <InputField
-//     label="This is a label"
-//     type={'text'}
-//     placeholder="Placeholder"
-//     touched={true}
-//     error="This is an error"
-//   />
-// );
-// withError.story = {
-//   name: 'With Error'
-// };
+export const selectFieldWithError = (): ReactElement => (
+  <SelectField
+    name="selectFieldWithError"
+    label="This one has value selected"
+    placeholder="Placeholder"
+    currentValue={undefined}
+    touched={true}
+    error={'This is a sample error message'}
+    options={options}
+  />
+);
+selectFieldWithError.story = {
+  name: 'With Error'
+};
