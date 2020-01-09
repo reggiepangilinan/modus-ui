4cb8ee71da7d028af31d1d13a8acae04356d4480
Reggie Pangilinan <reggiepangilinan@gmail.com>
2020-01-09 19:45:16 +1100

    Feature/add generator (#28)
    * Add component generator
    
    * Add template for mdx stories
    
    * Add template for mdx stories
    
    Co-authored-by: Reggie <46663727+reggie-it@users.noreply.github.com>

--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 module.exports = function(plop) {
+  plop.setHelper('wrapBracket', function(text) {
+    return `{${text}}`;
+  });
+
   plop.setGenerator('component', {
     description: 'Creates a new modus-ui component',
     prompts: [
@@ -9,6 +13,7 @@ module.exports = function(plop) {
         message: 'The name of the component please',
         validate: value => {
           if (!value) return 'name is required';
+          return true;
         }
       }
     ],
@@ -24,6 +29,12 @@ module.exports = function(plop) {
         path:
           'src/components/{{lowerCase name}}/{{lowerCase name}}.styles.scss',
         templateFile: 'plop/templates/component/component.styles.scss.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          'src/components/{{lowerCase name}}/{{lowerCase name}}.stories.mdx',
+        templateFile: 'plop/templates/component/component.stories.mdx.hbs'
       }
     ]
   });
