0a4f47c9d8f0bf235bbad638fa7e1e7dec80e863
reggie@bizbox.ph <reggiepangilinan@gmail.com>
2020-02-21 00:05:20 +1100

    Add card outline mode + refactor tab style

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -4,14 +4,22 @@ import styles from './card.styles.scss';
 
 export type CardProps = {
   id?: string;
-  width?: number;
   type?: 'default' | 'outline' | undefined;
   style?: CSSProperties;
 } & { children: ReactNode };
 
+const getStyle = (type: 'default' | 'outline' | undefined): string => {
+  if (type && type === 'outline') return 'cardOutline';
+  return 'cardDefault';
+};
+
 export const Card: FC<CardProps> = (props: CardProps): ReactElement => {
   return (
-    <div id={props.id} className={styles.cardDefault} style={props.style}>
+    <div
+      id={props.id}
+      className={styles[getStyle(props.type)]}
+      style={props.style}
+    >
       {props.children}
     </div>
   );
---
+++ src/components/card/stories/cardWithTabs.tsx
@@ -0,0 +1,44 @@
+import React, { FunctionComponent, ReactElement, useState } from 'react';
+
+import { TabItem, Tabs } from '../../tabs/tabs.component';
+import { Card } from '../card.component';
+
+export type CardWithTabsProps = {};
+
+export const CardWithTabs: FunctionComponent<
+  CardWithTabsProps
+> = (): ReactElement => {
+  const [currentTab, setcurrentTab] = useState(1);
+  const contentStyle = {
+    display: 'flex',
+    margin: '100px',
+    justifyContent: 'center',
+    fontWeight: 800
+  };
+  return (
+    <Card>
+      <Tabs>
+        <TabItem
+          label={'Tito'}
+          active={currentTab === 1}
+          onClick={(): void => setcurrentTab(1)}
+        />
+        <TabItem
+          label={'Vic'}
+          active={currentTab === 2}
+          onClick={(): void => setcurrentTab(2)}
+        />
+        <TabItem
+          label={'Joey'}
+          active={currentTab === 3}
+          onClick={(): void => setcurrentTab(3)}
+        />
+      </Tabs>
+      {currentTab === 1 && (
+        <p style={contentStyle}>{`Can't you see I'm trying`}</p>
+      )}
+      {currentTab === 2 && <p style={contentStyle}>{`I don't even like it`}</p>}
+      {currentTab === 3 && <p style={contentStyle}>{`Is this it?`}</p>}
+    </Card>
+  );
+};
